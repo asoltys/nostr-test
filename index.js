acf9733dd991587fc276692e2a3c0d8d6804e170
Adam Soltys <asoltys@gmail.com>
2022-12-19 03:05:24 -0800

    listen to all relays and forward

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-import { calculateId, signId, RelayPool } from "nostr";
+import { Relay, RelayPool } from "nostr";
+import { got } from "got";
 
-const coinos = "wss://nostr.coinos.io";
-const relays = [coinos];
-
+const { relays } = await got("https://nostr.watch/relays.json").json();
 const pool = RelayPool(relays);
 
-let privkey =
-  "98d6d44f5ce77848380a89cad93a550686a8cea50fdd3da2a825a2e3c46891ba";
-
-let event = {
-  pubkey: "a5cf19020a5973571d4538a3c45e62faea258fb17256480e4ab70c4da7ac483f",
-  created_at: Math.floor(Date.now() / 1000),
-  kind: 1,
-  content: "cool",
-  tags: [],
-};
-
-event.id = await calculateId(event);
-event.sig = await signId(privkey, event.id);
-
+let coinos;
 pool.on("open", (relay) => {
+  if (relay.url.includes("coinos")) coinos = relay;
   relay.subscribe("subid", { limit: 1 });
-
-  let send = () => {
-    console.log("sending", event);
-    relay.send(["EVENT", event]);
-  };
-
-  // wait a second then send
-  setTimeout(send, 1000);
 });
 
+let seen = [];
 pool.on("event", (relay, sub_id, ev) => {
-  console.log(ev);
+  if (seen.includes(ev.id)) return;
+  seen.push(ev.id);
+  seen.length > 1000 && seen.shift();
+
+  if (
+    coinos &&
+    ev.kind < 5 &&
+    !ev.content.startsWith("test") &&
+    !ev.content.startsWith("running branle")
+  ) {
+    let parsed;
+    try {
+      parsed = JSON.parse(ev.content);
+    } catch (e) {
+      parsed = ev.content;
+    }
+
+    coinos.send(["EVENT", ev]);
+    if (ev.kind === 1)
+      console.log(formatDate(new Date(ev.created_at * 1000)), parsed);
+  }
 });
+
+function formatDate(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutes;
+  return date.getMonth() + 1 + "/" + date.getDate() + " " + strTime;
+}
